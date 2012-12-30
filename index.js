03a0de7dd5250e074e80edf9dab8cc9708f6b632
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-30 23:12:10 +0100

    injectable, + add listening, connecting events

--- index.js
+++ index.js
@@ -1,42 +1,62 @@
-var net  = require('net')
+var net  = process.title === 'browser' ? null : (require)('net')
 var EventEmitter = require('events').EventEmitter
 
-module.exports = autonode
+module.exports = inject(net)
 
-function delay(fun) {
-  return function () {
-    var args = [].slice.call(arguments)
-    setTimeout(function () {
-       fun.apply(null, args)
-    }, 50)
-  }
-}
+function inject (net) {
 
-function autonode (handler) {
-  var emitter = new EventEmitter()
-  var server, client, _port
-  emitter.listen = 
-    function listen (port) {
-      //try and start a server on port.
-      server = net.createServer(onConnect).listen(port || _port)
-      server.once('error', function (err) {
-        //someone else has already started server.
-        //connect to it.
-        if('EADDRINUSE' == err.code) {
-          client = net.connect(port || _port)
-            .once('connect', onConnect)
-            .once('close', delay(listen))
-        }
-      })
-      //remember the port for next time.
-      _port = port
-      return this
+  /*function delay(fun) {
+    return function () {
+      var args = [].slice.call(arguments)
+      setTimeout(function () {
+         fun.apply(null, args)
+      }, 50)
     }
+  }*/
 
-  function onConnect (stream) {
-    emitter.emit('connection', stream || client)
+  function autonode (handler) {
+
+    var emitter = new EventEmitter()
+    var server, client, _port
+    emitter.listen = 
+      function listen (port) {
+        //try and start a server on port.
+        console.log('PORT:', port || _port)
+        server = net.createServer(onConnect).listen(port || _port)
+        server.once('error', function (err) {
+          //someone else has already started server.
+          //connect to it.
+          if('EADDRINUSE' == err.code) {
+            function reconnect () {
+              client.removeListener('error', reconnect)
+              client.removeListener('close', reconnect)
+              client.removeListener('end',   reconnect)
+              emitter.emit('connecting')
+              setTimeout(listen, 50)
+            }
+            client = net.connect(port || _port)
+              .once('connect', onConnect)
+              .once('error', reconnect)
+              .once('end',   reconnect)
+              .once('close', reconnect)
+          }
+        })
+        server.once('listening', function () {
+          emitter.emit('listening')
+        })
+        //remember the port for next time.
+        _port = port || _port
+        return this
+      }
+
+    function onConnect (stream) {
+      //'connection', stream, isServer
+      emitter.emit('connection', stream || client, stream ? true : false)
+    }
+    if(handler) emitter.on('connection', handler)
+    return emitter
   }
-  if(handler) emitter.on('connection', handler)
-  return emitter
-}
 
+  autonode.inject = inject
+  return autonode
+}
